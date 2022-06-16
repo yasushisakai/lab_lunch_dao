6551254378e898ba72c084e82074029fbd0abaee
Yasushi Sakai <yasushi.accounts@fastmail.com>
2022-06-16 10:57:07 -0400

    added 'app' submodule

--- programs/lab_lunch_dao/src/lib.rs
+++ programs/lab_lunch_dao/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9MM4PS44D5KZEVa1ruVuW14B5kbcupm2QXP1Gjfm3V7P");
 
 #[program]
 pub mod lab_lunch_dao {
